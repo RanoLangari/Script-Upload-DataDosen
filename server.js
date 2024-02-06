4068c70490d83ca8a0fee69b91a30dfb8774d60f
RanoLangari <langarirano@gmail.com>
2024-02-07 03:16:20 +0800

    -

--- server.js
+++ server.js
@@ -21,8 +21,12 @@ app.post("/uploadDataDosen", upload.single("file"), async (req, res) => {
       res.status(400).send("No file uploaded");
       return;
     }
+
+    // Read the uploaded file
     const buffer = await readFile(req.file.path);
     const workbook = xlsx.read(buffer, { type: "buffer" });
+
+    // Convert the first worksheet to JSON with first colomn key is nama and second colomn key is jurusan
     const worksheet = workbook.Sheets[workbook.SheetNames[0]];
     const data = xlsx.utils.sheet_to_json(worksheet, { header: 1 });
     const dosen = data.map((item) => {
@@ -31,10 +35,15 @@ app.post("/uploadDataDosen", upload.single("file"), async (req, res) => {
         jurusan: item[1],
       };
     });
+
+    // Insert the JSON data to Firestore using loop
     for (let i = 0; i < dosen.length; i++) {
       await db.collection("dosen").add(dosen[i]);
     }
+
+    // Delete the uploaded file
     fs.unlinkSync(req.file.path);
+
     res.send("Data uploaded successfully");
   } catch (error) {
     console.error("error pada uploadDataDosen: ", error);
